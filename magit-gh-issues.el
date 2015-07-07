842e51e65cfcfa857760f4c2beae8014a9ce5369
Christopher Reichert <creichert07@gmail.com>
2015-07-07 03:44:55 -0500

    Add ert tests for url parsing.
    * Tests derived from sigma magit-gh-pulls.

--- magit-gh-issues.el
+++ magit-gh-issues.el
@@ -255,6 +255,34 @@
   "Unconditionally turn on `magit-issues-mode'."
   (magit-gh-issues-mode 1))
 
+;; Tests
+(ert-deftest test-magit-gh-issues-parse-url-git-at ()
+  (should (equal '("creichert" . "magit-gh-issues")
+                 (magit-gh-issues-parse-url "git@github.com:creichert/magit-gh-issues.git"))))
+
+(ert-deftest test-magit-gh-issues-parse-url-https ()
+  (should (equal '("creichert" . "magit-gh-issues")
+                 (magit-gh-issues-parse-url "https://github.com/creichert/magit-gh-issues.git"))))
+
+(ert-deftest test-magit-gh-issues-parse-url-https ()
+  (should (equal '("creichert" . "magit-gh-issues")
+                 (magit-gh-issues-parse-url "https://github.com/creichert/magit-gh-issues/"))))
+
+(ert-deftest test-magit-gh-issues-parse-url-http ()
+  (should (equal '("creichert" . "magit-gh-issues")
+                 (magit-gh-issues-parse-url "http://github.com/creichert/magit-gh-issues.git"))))
+
+(ert-deftest test-magit-gh-issues-parse-url-git ()
+  (should (equal '("creichert" . "magit-gh-issues")
+                 (magit-gh-issues-parse-url "git://github.com/creichert/magit-gh-issues.git"))))
+
+(ert-deftest test-magic-gh-pulls-parse-url-invalid ()
+  (should (eq nil (magit-gh-issues-parse-url "http://google.com"))))
+
+(ert-deftest test-magic-gh-pulls-parse-url-garbage ()
+  (should (eq nil (magit-gh-issues-parse-url "sfalwkerwe09"))))
+
+
 (provide 'magit-gh-issues)
 
 ;; Local Variables:
